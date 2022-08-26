e92e98c3734df373954de3367d95d1849edc6f48
Wiktor <wiktorkoscielny@gmail.com>
2022-08-26 20:15:29 +0200

    Created switch function to display imported components in JSX

--- src/components/startpage/StartPage.js
+++ src/components/startpage/StartPage.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// components
+import AllCateg from './AllCateg'
+import TechCateg from './TechCateg'
+import ClothesCateg from './ClothesCateg';
+
 // styles in css for now
 import './StartPage.css'
 
@@ -7,35 +12,30 @@ class StartPage extends React.PureComponent { // eslint-disable-next-line
   constructor(props){
     super(props);
     this.state = {
-      currency: '',
-      products: []
+      currency: ''
     }
   }
   componentDidMount = async () => {
     const currencyData = this.props.currencyData
     this.setState({
-      currency: currencyData,
-      // products: allCateg
+      ...this.state,
+      currency: currencyData
     })
   }
-
+  renderSwitch = () => {
+    switch(this.props.currentCategory) {
+      case 'all':
+        return <AllCateg allCateg={this.props.allCateg} />;
+      case 'tech':
+        return <TechCateg techCateg={this.props.techCateg} />;
+      case 'clothes':
+        return <ClothesCateg clothesCateg={this.props.clothesCateg} />
+      }
+  }
   render() {
     return (
-      
       <section>
-        <div className='temporary__wrapper'>Current Currency: {this.props.currencyData}</div>
-        {this.props.allCateg ? 
-          <ul>
-          {this.props.allCateg.map((item, index) => {
-            return(
-              <li key={index}>
-                {item[0].brand}
-                <img src={item[0].gallery}/>
-              </li>
-            )
-          })}
-          </ul>
-        : 'Loading....'}
+        {this.renderSwitch()}
       </section>    
     );
   } 
