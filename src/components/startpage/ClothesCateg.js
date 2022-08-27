b0cbbde2aa3955cec92cc672dc64288b1acef735
Wiktor <wiktorkoscielny@gmail.com>
2022-08-27 19:37:58 +0200

    Wrapped JSX with styled components, created switch function to display price at choosen currency

--- src/components/startpage/ClothesCateg.js
+++ src/components/startpage/ClothesCateg.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+// styles
+import { ListWrapper, ListItem, ImgWrapper } from './Styles';
 export default class ClothesCateg extends Component {
     constructor(props) {
         super(props);
@@ -13,19 +15,37 @@ export default class ClothesCateg extends Component {
             clothesCateg: categ
         })
     }
+    currencySwitcher = (param) => {
+        switch (this.props.currencyData) {
+            case '$':
+                return <>{param.prices[0].amount}</>
+            case '£':
+                return <>{param.prices[1].amount}</>
+            case 'A$':
+                return <>{param.prices[2].amount}</>
+            case '¥':
+                return <>{param.prices[3].amount}</>
+            case '₽':
+                return <>{param.prices[4].amount}</>
+        }
+    }
     render() {
         return (
             <>
-                <ul>
+                <ListWrapper>
                     {this.props.clothesCateg.map((item, index) => {
                         return (
-                            <li key={index}>
-                                {item[0].brand}
-                                <img src={item[0].gallery} />
-                            </li>
+                            <ListItem key={index}>
+                                <ImgWrapper src={item[0].gallery} style={{ width: '100%', height: '80%' }}></ImgWrapper>
+                                <p>{item[0].name}</p>
+                                <p>
+                                    {this.props.currencyData}
+                                    {this.currencySwitcher(item[0])}
+                                </p>
+                            </ListItem>
                         )
                     })}
-                </ul>
+                </ListWrapper>
             </>
         )
     }
--- src/components/startpage/TechCateg.js
+++ src/components/startpage/TechCateg.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+// styles
+import { ListWrapper, ListItem, ImgWrapper } from './Styles';
+
 export default class TechCateg extends Component {
     constructor(props) {
         super(props);
@@ -13,19 +16,37 @@ export default class TechCateg extends Component {
             techCateg: categ
         })
     }
+    currencySwitcher = (param) => {
+        switch (this.props.currencyData) {
+            case '$':
+                return <>{param.prices[0].amount}</>
+            case '£':
+                return <>{param.prices[1].amount}</>
+            case 'A$':
+                return <>{param.prices[2].amount}</>
+            case '¥':
+                return <>{param.prices[3].amount}</>
+            case '₽':
+                return <>{param.prices[4].amount}</>
+        }
+    }
     render() {
         return (
             <>
-                <ul>
+                <ListWrapper>
                     {this.props.techCateg.map((item, index) => {
                         return (
-                            <li key={index}>
-                                {item[0].brand}
-                                <img src={item[0].gallery} />
-                            </li>
+                            <ListItem key={index}>
+                                <ImgWrapper src={item[0].gallery} style={{ width: '100%', height: '80%' }}></ImgWrapper>
+                                <p>{item[0].name}</p>
+                                <p>
+                                    {this.props.currencyData}
+                                    {this.currencySwitcher(item[0])}
+                                </p>
+                            </ListItem>
                         )
                     })}
-                </ul>
+                </ListWrapper>
             </>
         )
     }
