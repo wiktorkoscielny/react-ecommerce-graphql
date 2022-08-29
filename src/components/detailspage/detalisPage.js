6c305bd3db72c8c9d2fd09ff516ec6b7e8981d50
Wiktor <wiktorkoscielny@gmail.com>
2022-08-29 12:59:17 +0200

    Created component where i have passed data of product that has been clicked on homepage. I displayed individual product data sent as props: directly or via the map function, in one case I also mapped over nestem array of objects.

--- src/components/detailspage/detalisPage.js
+++ src/components/detailspage/detalisPage.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react'
 
-export default class detailsPage extends Component {
+// styled components
+import { SectionsWrapper, LeftSection, MiddleSection, RightSection } from './Styles';
+
+export default class DetailsPage extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        
-      }
+        super(props);
+        this.state = {
+            productData: {},
+            currentPhoto: '',
+            productSpec: {}
+        }
+    }
+    componentDidMount() {
+        const productData = this.props.productData
+        const productSpec = this.props.productData.attributes
+        this.setState({
+            ...this.state,
+            productData: productData,
+            currentPhoto: productData.gallery[0],
+            productSpec: productSpec
+        })
+    }
+    changePhoto = (event) => {
+        this.setState({
+            currentPhoto: event
+        })
     }
     render() {
-        return(
-            <>
-            </>
+        const propsData = this.props.productData
+        const productGallery = this.props.productData.gallery
+        const productSpec = this.props.productData.attributes
+        return (
+            <section>
+                <SectionsWrapper>
+                    <LeftSection>
+                        {productGallery.map((item, index) => {
+                            return (
+                                <ul key={index}>
+                                    <li>
+                                        <img src={item} alt='product picture'></img>
+                                    </li>
+                                </ul>
+                            )
+                        })}
+                    </LeftSection>
+                    <MiddleSection>{this.state.currentPhoto && <img src={this.state.currentPhoto} alt='big picture of product'></img>}</MiddleSection>
+                    <RightSection>
+                        {propsData.brand}{propsData.name}
+                        {productSpec.map((item, index) => {
+                            return (
+                                <div key={index}>
+                                    <h1>{item.id}</h1>
+                                    <ul>
+                                        {item.items.map((a, c) => {
+                                            return (
+                                                <li key={c}>
+                                                    {a.id}
+                                                    {a.value}
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+                            )
+                        })}
+                    </RightSection>
+                </SectionsWrapper>
+            </section>
         )
     }
 }
