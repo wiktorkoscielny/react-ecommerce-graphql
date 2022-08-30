d00aef25c93992c4ca5b615185b2fb64977fa8cd
Wiktor <wiktorkoscielny@gmail.com>
2022-08-30 13:56:03 +0200

    Changed style of components depending on displayed (mapped) data passed as props

--- src/components/detailspage/Styles.js
+++ src/components/detailspage/Styles.js
@@ -49,15 +49,25 @@ export const Button = styled.button`
     background: var(--light-green);
     border: 0px;
 `
+
 export const ListOfOptions = styled.button`
-    min-width: 63px;
-    height: 45px;
+    min-width: ${({ param1 }) => (param1 === 'Color' ? '32px' : '63px')};
+    width: ${({ param1 }) => (param1 === 'Color' ? '32px' : 'auto')};
+    height: ${({ param1 }) => (param1 === 'Color' ? '32px' : '45px')};
     text-align: center;
     margin-right: 5px;
-    background: ${({ param1, param2, currentOption }) => (currentOption === param1 + param2 ? 'var(--text-dark)' : 'var(--bg-primary)')};
+    background: ${({ param1, param2, currentOption }) => (currentOption === param1 + param2 && param1 !== 'Color' ? 'var(--text-dark)' : 'var(--bg-primary)')};
+    background: ${({ param1, color }) => (param1 === 'Color' ? color : color)};
     color: ${({ param1, param2, currentOption }) => (currentOption === param1 + param2 ? 'var(--bg-primary)' : 'var(--text-dark)')};
     font-size: 16px;
-    border: 1px solid var(--text-dark);
+    border: ${({ param1 }) => (param1 === 'Color' ? '1px solid var(--bg-primary)' : '1px solid var(--text-dark)')};
+    outline: ${({ param1, param2, currentOption }) => (currentOption === param1 + param2 && param1 === 'Color' ? '2px solid var(--light-green)' : null)};
     font-family: 'Source Sans Pro', sans-serif;
     cursor: pointer;
+`
+export const ListOfColors = styled.div`
+    height: 32px;
+    width: 32px;
+    background: red;
+    border: 0px;
 `
