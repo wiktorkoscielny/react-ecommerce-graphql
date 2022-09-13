e7eada257eb9f1db17322866c7be13e31ecba871
Wiktor <wiktorkoscielny@gmail.com>
2022-09-13 15:01:34 +0200

    Changed style of link, added hover effects and changed style of mapped list

--- src/components/startpage/Styles.js
+++ src/components/startpage/Styles.js
@@ -1,13 +1,22 @@
 import styled from 'styled-components';
-
+import { Link } from 'react-router-dom'
+export const MainWrapper = styled.div`
+    margin-top: 80px;
+    margin-bottom: 100px;
+        h1 {
+            height: 50px;
+            font-size: 42px;
+            line-height: 67.2px;
+            font-weight: 400;
+        }
+`
 export const ListWrapper = styled.ul`
-    padding: 100px 0 0 0;
+    padding: 50px 0 0 0;
     margin: 0;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 2rem;
-    text-align: center;
-
+    
     @media screen and (max-width: 890px) {
         grid-template-columns: repeat(2, 1fr);
     }
@@ -17,20 +26,61 @@ export const ListWrapper = styled.ul`
 `
 export const ListItem = styled.li`
     list-style: none;
+    max-width: 248px
     display: inline;
-    text-align: center;
+    position: relative;
     cursor: pointer;
     padding: 0 0 50px 0;
+    text-align: center;
+`
+export const ImgWrapper = styled.div`
+    min-height: 310px;
+    img {
+        width: 310px;
+        max-height: 310px;
+        filter: brightness(0.99);
+    }
+`
+export const TextWrapper = styled.div`
+    width: 310px;
+    margin-left: auto;
+    margin-right: auto;
     p {
         font-size: 18px;
         font-family: 'Raleway';
         line-height: 28.8px;
-        display: flex;
-        align-items: center;
+        text-align: left;
         color: var(--text-dark);
+        max-width: 248px;
     }
 `
-export const ImgWrapper = styled.img`
-    max-width: 100%;
-    height: 282px;
+export const FloatingCart = styled.div`
+    visibility: hidden;
+    position: absolute;
+    top:280px;
+    right: 10px;
+    height: 52px;
+    width: 52px;
+    z-index: 30;
+    border: 1px solid transparent;
+    border-radius: 50%;
+    background: var(--light-green);
+        img {
+            position: relative;
+            margin-left: auto;
+            margin-right: auto;
+            top: 15px;
+
+        }
+`
+export const StyledLink = styled(Link)`
+    border: 20px solid transparent;
+    text-decoration: none;
+    :hover {
+        -webkit-box-shadow: 0px 0px 14px 5px rgba(0,0,0,0.1); 
+        box-shadow: 0px 0px 14px 5px rgba(0,0,0,0.1);
+    }
+    &:hover ${FloatingCart} {
+        visibility: visible;
+    }
 `
