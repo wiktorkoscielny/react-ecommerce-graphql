e8267131b71c708666eaec360bffbfea7728a675
Wiktor <wiktorkoscielny@gmail.com>
2022-09-22 18:25:32 +0200

    Changed field list in query

--- src/queries/GetCategory.js
+++ src/queries/GetCategory.js
@@ -6,7 +6,7 @@ const getCategory = async (category) => {
 
   const queryCategory = new Query("category", true)
     .addArgument("input", "CategoryInput", { title : category})
-    .addField(new Field("products", true).addFieldList(["id", "name", "brand", "attributes{id, items{value, id}}", "inStock", "gallery", "prices{amount}"]))    
+    .addField(new Field("products", true).addFieldList(["id", "name", "inStock", "gallery", "description", "brand", "attributes {id, items {value, id}}", "prices {amount}"]))    
 
     return await client.post(queryCategory)
   }
